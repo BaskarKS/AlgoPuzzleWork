50a52190c23dd924c2b0b1f1fc51dbe95d73cf24
Baskar K S <baskarks@gmail.com>
2019-12-13 15:28:46 +0530

    GetNthFibonnaci Problem Code solution

--- src/com/learning/algo/expert/AlgoExpert.java
+++ src/com/learning/algo/expert/AlgoExpert.java
@@ -530,6 +530,72 @@ public class AlgoExpert {
         binarySearchTree.insert(5).insert(15).insert(5).insert(2).insert(14).insert(22);
         binarySearchTree.contains(5);
     }
+    public static int getNthFib(int n) {
+        // Write your code here.
+        if (n < 0)
+            return -1;
+        if (n == 1)
+            return 1;
+        if (n == 0)
+            return 0;
+        return getNthFib(n-1) + getNthFib(n-2);
+    }
+    public static int getNthFibIterative(int n) {
+        int result = 0, prev = 1, lastPrev = 0, index = 2;
+        while (index <= n) {
+            result = prev + lastPrev;
+            lastPrev = prev;
+            prev = result;
+            index++;
+        }
+        return result;
+    }
+    public static int getNthFibDynamicProg(int n, HashMap<Integer, Integer> fibValues) {
+        int result = 0, index = 2;
+        if (!fibValues.containsKey(0)) fibValues.put(0, 0);
+        if (!fibValues.containsKey(1)) fibValues.put(1, 1);
+        if (!fibValues.containsKey(n)) {
+            while (index <= n) {
+                if (fibValues.containsKey(index)) {
+                    index++;
+                    continue;
+                }
+                result = fibValues.get(index - 1) + fibValues.get(index - 2);
+                fibValues.put(index, result);
+                index++;
+            }
+        }
+        return fibValues.get(n);
+    }
+    public static int getNthFibDynamicProgNoCache(int n) {
+        int result = 0, index = 2;
+        Map<Integer, Integer> fibCache = new HashMap<>();
+        fibCache.put(0, 0);
+        fibCache.put(1, 1);
+        while (index <= n) {
+            result = fibCache.get(index - 1) + fibCache.get(index - 2);
+            fibCache.put(index, result);
+            index++;
+        }
+        return fibCache.get(n);
+    }
+    //Time Complexity - O(n) - Space Complexity - O(n)
+    public static int getNthFibDynamicProgRecursive(int n) {
+        Map<Integer, Integer> fibCache = new HashMap<>();
+        fibCache.put(0, 0);
+        fibCache.put(1, 1);
+        return getNthFibRecursive(n, fibCache);
+    }
+    //0  1  2  3  4  5  6  7  8   9
+    //0  1  1  2  3  5  8  13  21 34
+    public static int getNthFibRecursive(int n, Map<Integer, Integer> fibCache) {
+        if (fibCache.containsKey(n))
+            return fibCache.get(n);
+        else
+            fibCache.put(n, getNthFibRecursive(n - 1, fibCache)
+                    + getNthFibRecursive(n - 2, fibCache));
+        return fibCache.get(n);
+    }
 }
 
 
--- src/com/learning/algo/expert/Main.java
+++ src/com/learning/algo/expert/Main.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
         String pi = "3141592653589793238462643383279";
@@ -7,6 +9,18 @@ public class Main {
                 "4", "793"};
         String[] numbers2 = {"3", "1", "4", "592", "65", "55", "35", "8", "9793", "2384626", "83279"};
         int minSeparators = AlgoExpert.numbersInPiBest2(pi, numbers2);
-        System.out.println(minSeparators);
+        //System.out.println(minSeparators);
+
+        //0  1  2  3  4  5  6  7  8   9
+        //0  1  1  2  3  5  8  13  21 34
+        int fibInput = 9;
+        int fibRet = AlgoExpert.getNthFibDynamicProgRecursive(fibInput);
+        System.out.println("Fib Result : " + fibRet);
+        HashMap<Integer, Integer> fibCache = new HashMap<>();
+        int fibResult = AlgoExpert.getNthFibDynamicProg(fibInput, fibCache);
+        System.out.println("Fib Dynamic : " + fibInput +" : " + fibResult);
+        int newFibInput = 5;
+        int fibResultOne = AlgoExpert.getNthFibDynamicProg(newFibInput, fibCache);
+        System.out.println("Fib Dynamic : " + newFibInput +" : " + fibResultOne);
     }
 }
